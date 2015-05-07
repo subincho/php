8f29bf1bc0770a624a6b4aa7b28feb15856befc4
subincho <subincho@abv.bg>
2015-05-07 17:07:29 +0300

    css

---
+++ 03-css/index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Kalin</title>
+        <link rel="stylesheet" type="text/css" href="img.css" />
+    </head>
+    <body>
+		<section id="content">
+			<div class="img">
+				<a target="_blank" href="/gallery/shtrang_vertikalen.JPG">
+					<img src="/gallery/shtrang_vertikalen.JPG" alt="shtrang vertikalen.JPG" />
+				</a>
+				<div class="desc">вертикален щранг</div>
+			</div>
+			<div class="img1">
+				<a target="_blank" href="/gallery/shtrangove.JPG">
+					<img src="/gallery/shtrangove.JPG" alt="shtrangove.JPG" />
+				</a>
+				<div class="desc1">хоризонтален щранг</div>
+			</div>
+			<div class="clearfix"></div>
+        </section>
+	</body>
+</html>
